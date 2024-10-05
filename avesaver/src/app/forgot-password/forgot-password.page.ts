851a2bd801950724fa743d178d665ebfac5593a3
Sofia Higa <sofihiga95@gmail.com>
2024-10-04 21:24:52 -0300

    Agregado de funcion para recuperar contraseña y btn provisorio para cerrar sesión en menu hamburguesa

--- avesaver/src/app/forgot-password/forgot-password.page.ts
+++ avesaver/src/app/forgot-password/forgot-password.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,10 +12,47 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordPage implements OnInit {
 
+  email: string = '';
+
   constructor(
     public auth: AuthenticationService,
-    public router: Router
+    public router: Router,
+    private toastController: ToastController
   ) { }
+
+  async recuperarContra() {
+    try {
+      const auth = getAuth(); //
+      await sendPasswordResetEmail(auth, this.email);
+      await this.presentToast('Contraseña recuperada: Link de recuperación enviado a su correo electrónico');
+      setTimeout(() => {
+        this.router.navigate(['login']); // 
+      }, 2000);
+    } catch (error: any) {
+      if ( error.code === 'auth/user-not-found') {
+        console.log('Correo electrónico no registrado:', error);
+        await this.presentToast('Error: Correo electrónico no registrado');
+      } else {
+        // Otros errores generales
+        console.log('Error al enviar el correo electrónico de restablecimiento:', error);
+        await this.presentToast('Error: No se pudo enviar el correo electrónico de restablecimiento');
+      }
+    }
+  }
+
+    /**
+   * @function presentToast
+   * @param mensage  tipo string, recibe el mensaje a mostrar en pantalla
+   * @description crea y muestra en pantalla una advertencia
+   */
+    async presentToast(message: string) {
+      const toast = await this.toastController.create({
+        message,
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    }
 
   ngOnInit() {
   }
