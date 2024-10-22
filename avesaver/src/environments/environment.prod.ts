06826b0c10c5191234afaf8bca6ba3cd75335138
MatiCampo96 <maticampopiano@hotmail.com>
2024-10-22 17:12:26 -0300

    confirmacion pre pull

--- avesaver/src/environments/environment.prod.ts
+++ avesaver/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA30VDXjsn8oRlcXtHTIWjwQB-zEj9scMM",
+    authDomain: "aves-a-ver.firebaseapp.com",
+    projectId: "aves-a-ver",
+    storageBucket: "gs://aves-a-ver.appspot.com",
+    messagingSenderId: "847890763137",
+    appId: "1:847890763137:web:a95d5c741559b7fab4ac80"
+  }
+  
 };
